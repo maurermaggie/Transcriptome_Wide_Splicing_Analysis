48f48d8a67c895d7d0b023d6bd1f03eeb0be3bfc
Maggie Maurer Arriaga <87448603+maurermaggie@users.noreply.github.com>
2025-01-06 10:56:23 -0800

    Delete misc_scripts directory

--- misc_scripts/notebooks/batch_reconciliation.r
+++
@@ -1,8 +0,0 @@
-library(readxl)
-library(magrittr)
-library(tidyverse)
-require(data.table)
-
-metadata <- read_csv("/home/maurertm/smontgom/shared/UDN/Analysis/FRASER/inputs/metadata/FRASER_metadata.csv")
-
-no_batch <- metadata %>% filter(is.na(batch))
